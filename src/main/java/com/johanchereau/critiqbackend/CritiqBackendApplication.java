75aecca91593d16b47e14f21764ae5645060c982
Johan Chereau <johanchereau2@gmail.com>
2025-03-14 11:44:02 +0100

    init: Spring Boot project with necessary dependencies

---
+++ src/main/java/com/johanchereau/critiqbackend/CritiqBackendApplication.java
@@ -0,0 +1,13 @@
+package com.johanchereau.critiqbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CritiqBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CritiqBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/johanchereau/critiqbackend/CritiqBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johanchereau.critiqbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CritiqBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
